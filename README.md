71056281726d5c461165645dc6cc0ab248c26c30
simpledevdima <skvdmt@yandex.ru>
2023-05-12 23:21:08 +0300

    add chuck function

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# slice
-Slicing functions
+# Slice-strings
+The package is a function to manage a slice of data containing strings.
+
+## Installation
+```
+go get github.com/simpledevdima/slice-strings
+```
+
+## Functions
+- Chuck - Chunk splits an array into parts;
+
+
+### Chuck
+```go
+Chuck (slice []string, length int, flags ...bool) ([][]string, error)
+```
+Chunk splits an array into parts.<br />
+Splits the array into multiple arrays of length elements.<br />
+The last array retrieved may contain fewer values than length.<br />
+The first argument to flags can be set to the boolean value true to indicate that a new native array should be created for the returned result.
+
+
+### Chuck example
+```go
+s := []string{"apple", "body", "cloud", "diamond", "each"}
+chunk, err := slice.Chunk(s, 2, true)
+if err != nil {
+    log.Fatalln(err)
+}
+fmt.Println(chunk)
+```
+
+Result:
+```
+[[apple body] [cloud diamond] [each]]
+```
---
+++ slice.go
@@ -0,0 +1,39 @@
+// Package slice set of functions for working with slices
+package slice
+
+import (
+	"errors"
+	"math"
+)
+
+// NewNativeArray creates a new own array for the result
+func NewNativeArray(slice []string) []string {
+	newSlice := make([]string, len(slice))
+	copy(newSlice, slice)
+	return newSlice
+}
+
+// Chunk splits an array into parts.
+// Splits the array into multiple arrays of length elements.
+// The last array retrieved may contain fewer values than length.
+// The first argument to flags can be set to the boolean value true to indicate that a new native array should be created for the returned result.
+func Chunk(slice []string, length int, flags ...bool) ([][]string, error) {
+	// argument checking
+	if length <= 0 {
+		return nil, errors.New("length must be greater than zero")
+	}
+	if len(flags) > 0 && flags[0] {
+		slice = NewNativeArray(slice)
+	}
+	capacity := int(math.Ceil(float64(len(slice)) / float64(length)))
+	result := make([][]string, 0, capacity)
+	for part := 0; part < len(slice); part += length {
+		step := part + length
+		if step <= len(slice) {
+			result = append(result, slice[part:step])
+		} else {
+			result = append(result, slice[part:])
+		}
+	}
+	return result, nil
+}
